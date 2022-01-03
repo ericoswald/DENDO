f3f3b02a298499fbe6b73aca1761f3c161bdc53f
eric.oswald <oswald.eric99@gmail.com>
2022-01-03 16:06:46 +0100

    creation du footer

---
+++ footer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
+    <link rel="stylesheet" href="./style_footer.css" type="text/css" media="all">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap" rel="stylesheet">
+
+    <title>Dendo Jitesha</title>
+</head>
+
+<body>
+    <footer>
+        <div class="frst_part">
+            <div class="colonne_footer">
+                <h3>Contact</h3>
+                <span>Email Jitensha</span>
+                <span>N° téléphone</span>
+                <div class="social_icons">
+                    <span class="iconify" data-icon="akar-icons:instagram-fill"></span>
+                    <span class="iconify" data-icon="akar-icons:facebook-fill"></span></div>
+            </div>
+            <div class="colonne_footer">
+                <h3>Newsletter</h3>
+                <span>Retrouvez l'essentiel de l'actualité en vous inscrivant à la newsletter</span>
+                <input type="email" placeholder="Email">
+            </div>
+            <div class="colonne_footer">
+                <h3>Information</h3>
+                <span>FAQ</span>
+                <span>Support</span>
+                <span class="iconify" id="icon_chat" data-icon="akar-icons:chat-question"></span>
+            </div>
+        </div>
+        
+    </footer>
+</body>
+
+</html>
---
+++ testpage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
+    <link rel="stylesheet" href="style.css" type="text/css" media="all">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap" rel="stylesheet">
+
+    <title>Dendo Jitesha</title>
+</head>
+
+<body>
+<?php include('header.php'); ?> 
+<?php include('footer.php'); ?>  
+</body>
+</html>

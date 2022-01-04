381c521bf9d96acf0685c725e67f9f9784a43803
eric.oswald <oswald.eric99@gmail.com>
2022-01-04 14:59:20 +0100

    Menu integrer a une page mais probleme peut pas selection (surement un probleme avec les differents fichiers style integrer dans les includes)

---
+++ header_index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
+    <link rel="stylesheet" href="./style_header_index.css" type="text/css" media="all">
+    <title>Document</title>
+
+</head>
+
+<body>
+
+
+    <header>
+        <div class="header">
+            <div class="header_menu">
+                <button class="btn " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom"
+                    aria-controls="offcanvasBottom"><span class="iconify" data-icon="ci:menu-alt-02" data-width="20"
+                        data-height="20" data-flip="horizontal" style="color: white;"></span></button>
+
+                <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom"
+                    aria-labelledby="offcanvasBottomLabel">
+                    <div class="offcanvas-header">
+
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                            aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body small">
+                        
+                        <div class="dropdown">
+                            <button class="btn  " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                              PRODUITS
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                              <li><a class="dropdown-item" href="#">VELOS</a></li>
+                              <li><a class="dropdown-item" href="#">E-VELOS</a></li>
+                              <li><a class="dropdown-item" href="#">OCCASION - DESTOCKAGE</a></li>
+                              <li><a class="dropdown-item" href="#">EQUIPEMENT</a></li>
+                              <li><a class="dropdown-item" href="#">COMPOSANT ET ACCESSOIRE</a></li>
+                            </ul>
+                          </div>
+                          <div class="dropdown">
+                            <button class="btn  " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                              SERVICE
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                              <li><a class="dropdown-item" href="#">SUPPORT</a></li>
+                              <li><a class="dropdown-item" href="#">FAQ</a></li>
+                              <li><a class="dropdown-item" href="#">PAIEMENT ET EXPEDITION</a></li>
+                              <li><a class="dropdown-item" href="#">INFORMATIONS</a></li>
+                              <li><a class="dropdown-item" href="#">CONTACT</a></li>
+                              <li><a class="dropdown-item" href="#">TCHAT</a></li>
+                            </ul>
+                          </div>
+                          <div class="dropdown">
+                            <button class="btn  " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                              JITENSHA
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                              <li><a class="dropdown-item" href="#">A PROPOS</a></li>
+                              <li><a class="dropdown-item" href="#">TECHNOLOGIE</a></li>
+                              <li><a class="dropdown-item" href="#">TEMOIGNAGE</a></li>
+                              <li><a class="dropdown-item" href="#">PRESSE</a></li>
+                              <li><a class="dropdown-item" href="#">OÙ ACHETER ?</a></li>
+                            </ul>
+                          </div>
+                          <div class="dropdown">
+                            <button class="btn  " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                              MON COMPTE
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                              <li><a class="dropdown-item" href="#">Velos</a></li>
+                              <li><a class="dropdown-item" href="#">E-Velos</a></li>
+                              <li><a class="dropdown-item" href="#">Ocasion - Destockage</a></li>
+                              <li><a class="dropdown-item" href="#">Equipement</a></li>
+                              <li><a class="dropdown-item" href="#">Composant accessoire</a></li>
+                            </ul>
+                          </div>
+
+                          
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="header_close">
+                <span class="iconify" data-icon="ant-design:close-outlined"></span>
+                <span>FERMER</span>
+            </div> -->
+            <div class="header_logo"><img src="./assets/image/main.png" alt=""></div>
+            <div class="header_menu icons_menu">
+                <span class="iconify" data-icon="bi:search"></span>
+                <span class="iconify" data-icon="codicon:account"></span>
+                <span class="iconify" data-icon="grommet-icons:shop"></span>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
--- testpage.php
+++ testpage.php
@@ -2,22 +2,88 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
-    <link rel="stylesheet" href="style.css" type="text/css" media="all">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap" rel="stylesheet">
-
-    <title>Dendo Jitesha</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+  </script>
+  <title>Dendo Jitesha</title>
+  <link rel="stylesheet" href="style.css" type="text/css" media="all">
 </head>
 
 <body>
-<?php include('header.php'); ?> 
 
-<?php include('footer.php'); ?>  
+  <!-- CAROUSEL -->
+  <main>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="./assets/image/background.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+
+        <!-- <div class="carousel-item active">
+      <div class="img" style="background-image: url('./assets/image/background.jpg');">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+      </div>
+    </div> -->
+
+        <div class="carousel-item">
+          <img src="./assets/image/background.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="./assets/image/background.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </main>
+  
+  <?php include('header_index.php'); ?>
+  <?php include('footer.php'); ?>
+
+
+  
+
 </body>
+
 </html>

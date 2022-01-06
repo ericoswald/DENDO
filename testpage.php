960de3be3463594c0d25f1d8717f595e57342d3a
eric.oswald <oswald.eric99@gmail.com>
2022-01-06 15:08:33 +0100

    page d'acceuil fait rest a modifier qq trucs

--- testpage.php
+++ testpage.php
@@ -24,7 +24,7 @@
   <!-- CAROUSEL -->
   <main>
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
+      <div class="carousel-indicators aaaa">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
           aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
@@ -33,33 +33,26 @@
           aria-label="Slide 3"></button>
       </div>
       <div class="carousel-inner">
+
         <div class="carousel-item active">
           <img src="./assets/image/background.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+          <div class="carousel-caption aaaa d-none d-md-block">
+            <h1>JITENSHA</h1>
+            
           </div>
         </div>
 
-        <!-- <div class="carousel-item active">
-      <div class="img" style="background-image: url('./assets/image/background.jpg');">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-      </div>
-    </div> -->
-
         <div class="carousel-item">
           <img src="./assets/image/background.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
+          <div class="carousel-caption aaaa d-none d-md-block">
+            <h5>Présentation de l’entreprise</h5>
             <p>Some representative placeholder content for the second slide.</p>
           </div>
         </div>
+
         <div class="carousel-item">
           <img src="./assets/image/background.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
+          <div class="carousel-caption aaaa d-none d-md-block">
             <h5>Third slide label</h5>
             <p>Some representative placeholder content for the third slide.</p>
           </div>
